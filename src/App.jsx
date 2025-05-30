c88fa0782941dc6a4b6705f5c596da55529f8820
Juan Sierra <juansierra01@gmail.com>
2025-05-29 23:48:03 -0300

    gitpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import AuthContextProvider from './components/Auth/Auth'
 import ThemeProvider from './components/Theme/ThemeContext';
+
 import Navegacion from "./components/Navegacion/Navegacion";
 import Home from "./pages/Home/Home";
 import Login from "./components/Login/Login";
@@ -14,25 +15,24 @@ import Configuracion from "./pages/Configuracion/Configuracion";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
   return (
     <AuthContextProvider>
       <ThemeProvider>
-        <BrowserRouter>
-            <Navegacion />
-            <Routes>
-                <Route element={<RutasPublicas />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/Login" element={<Login />} />
-                    <Route path="/Productos" element={<Productos />} />
-                    <Route path="/Configuracion" element={<Configuracion />} />
-                </Route>
-                <Route element={<RutasPrivadas />}>
-                    <Route path="/logout" element={<Logout />} />
-                </Route>
-            </Routes>
-            <Footer />
+        <BrowserRouter basename="/Salumeria">
+          <Navegacion />
+          <Routes>
+            <Route element={<RutasPublicas />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Productos" element={<Productos />} />
+              <Route path="/Configuracion" element={<Configuracion />} />
+            </Route>
+            <Route element={<RutasPrivadas />}>
+              <Route path="/logout" element={<Logout />} />
+            </Route>
+          </Routes>
+          <Footer />
         </BrowserRouter>
       </ThemeProvider>
     </AuthContextProvider>
